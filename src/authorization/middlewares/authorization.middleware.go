1e4e0903df52d917b9e543ddec68c58e150ea92b
Bhfreagra <eberdellima@outlook.com>
2020-08-26 17:49:17 +0200

    Add check for access token expiration time

--- src/authorization/middlewares/authorization.middleware.go
+++ src/authorization/middlewares/authorization.middleware.go
@@ -6,6 +6,7 @@ import (
 	"go-moose/src/authorization/inputs"
 	"go-moose/src/authorization/services"
 	"net/http"
+	"time"
 
 	"github.com/dgrijalva/jwt-go"
 	"github.com/gin-gonic/gin"
@@ -68,18 +69,30 @@ func CheckJWT() gin.HandlerFunc {
 		token := ctx.GetHeader("Authorization")
 		token = string(token[7:])
 
+		// Parse token correctly
 		parsedToken, err := services.ValidateAccessToken(token)
 		if err != nil {
-			ctx.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"message": "Not authenticated"})
+			ctx.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"message": "Not authorized"})
 			return
 		}
 
+		// Assert token claims
 		claims, ok := parsedToken.Claims.(jwt.MapClaims)
 		if !ok || !parsedToken.Valid {
-			ctx.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"message": "Not authenticated"})
+			ctx.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"message": "Not authorized"})
 			return
 		}
 
+		// Check for expiration
+		exp := string(claims["exp"].(string))
+		expTime, err := time.Parse(time.RFC3339, exp)
+
+		if expTime.Before(time.Now()) {
+			ctx.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"message": "Not authorized"})
+			return
+		}
+
+		// Check if token exists in db
 		userID := uint(claims["user_id"].(float64))
 
 		var userToken models.UserToken
@@ -87,7 +100,7 @@ func CheckJWT() gin.HandlerFunc {
 		database.DB.Where(models.UserToken{UserID: userID, AccessToken: token}).Preload("User").First(&userToken)
 
 		if userToken.ID == 0 {
-			ctx.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"message": "Not authenticated"})
+			ctx.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"message": "Not authorized"})
 			return
 		}
 
