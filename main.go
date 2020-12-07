03522c3aad0a8ab9398661986db5d8454d6b3f93
Ergi <eberdellima@outlook.com>
2020-12-07 18:07:19 +0100

    Refactor main setup

--- main.go
+++ main.go
@@ -1,7 +1,8 @@
 package main
 
 import (
-	"context"
+	"fmt"
+	"github.com/joho/godotenv"
 	"go-moose/database"
 	"go-moose/src"
 	"log"
@@ -14,44 +15,48 @@ import (
 
 func main() {
 
+	if err := godotenv.Load(".env"); err != nil {
+		log.Fatal(err)
+	}
+
 	db := database.InitializeDBConnection()
 	defer db.Close()
 
+	router := src.ConfigureAPI()
+
+	errorChannel := make(chan error, 2)
+
+	go listenToPort(errorChannel, router)
+	go listenToSysCall(errorChannel)
+
+	fmt.Printf("\nTerminated: %s\n", <-errorChannel)
+}
+
+func setupPort() string {
+	port := "8000"
+
+	if os.Getenv("PORT") != "" {
+		port = os.Getenv("PORT")
+	}
+
+	return fmt.Sprintf(":%s", port)
+}
+
+func listenToPort(errorChannel chan<- error, handler http.Handler) {
+
 	server := &http.Server{
-		Addr:           "localhost:" + os.Getenv("PORT"),
-		Handler:        src.ConfigureAPI(),
+		Addr:           setupPort(),
+		Handler:        handler,
 		ReadTimeout:    10 * time.Second,
 		WriteTimeout:   10 * time.Second,
 		MaxHeaderBytes: 1 << 20,
 	}
 
-	go startServer(server)
-
-	// Wait for interrupt signal to gracefully shutdown the server with
-	// a timeout of 5 seconds.
-	quit := make(chan os.Signal)
-	// kill (no param) default send syscall.SIGTERM
-	// kill -2 is syscall.SIGINT
-	// kill -9 is syscall.SIGKILL but can't be catch, so don't need add it
-	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
-	<-quit
-	log.Println("Shutting down server...")
-
-	// The context is used to inform the server it has 5 seconds to finish
-	// the request it is currently handling
-	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-	defer cancel()
-	if err := server.Shutdown(ctx); err != nil {
-		log.Fatal("Server forced to shutdown:", err)
-	}
-
-	log.Println("Server exiting")
+	errorChannel <- server.ListenAndServe()
 }
 
-// Initializing the server in a goroutine so that
-// it won't block the graceful shutdown handling below
-func startServer(server *http.Server) {
-	if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
-		log.Fatalf("listen: %s\n", err)
-	}
+func listenToSysCall(errorChannel chan<- error) {
+	systemChannel := make(chan os.Signal, 1)
+	signal.Notify(systemChannel, syscall.SIGINT)
+	errorChannel <- fmt.Errorf("%s", <-systemChannel)
 }
